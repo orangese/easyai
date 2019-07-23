62e695c0ca26d3456c58197aff3fce78474c482f
Ryan <Ryan@Johns-MacBook-Pro.local>
2019-07-23 18:57:01 -0400

    adding src folder

--- src/easy_ai.py
+++ src/easy_ai.py
@@ -8,10 +8,8 @@ machine learning.
 
 """
 
-import tensorflow as tf
 import keras as K
-
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+from time import time
 
 class Input(object):
   """input layer, no keras mask"""
@@ -32,10 +30,10 @@ class Dense(object):
     self.k_mask = K.layers.Dense(units = self.num_neurons, input_shape = (self.prev.num_neurons, ),
                                  activation = self.actv)
 
-class Network(object):
+class NN(object):
   """uses wrapper classes to create a functional network-- wrapper for Sequential"""
 
-  def __init__(self, layers = [], cost = "crossentropy"):
+  def __init__(self, layers = [], cost = "mse"):
     self.layers = tuple(layers)
     self.cost = cost
     self.link_layers()
@@ -71,15 +69,22 @@ class Network(object):
     elif position: del new_layers[position]
     self.layers = new_layers
 
-  def train(self, x, y, lr = 3.0, epochs = 60, batch_size = 10):
+  def train(self, x, y, lr = 3.0, epochs = 1, batch_size = 10):
     """trains and compiles k_model-- simplified version of keras' compile and fit"""
     optimizer = K.optimizers.SGD(lr = lr)
-    self.k_model.compile(optimizer = optimizer, loss = self.cost, metrics = ["accuracy"])
+    metrics = ["categorical_accuracy"] if self.layers[-1].num_neurons != 1 else ["binary_accuracy"]
+    self.k_model.compile(optimizer = optimizer, loss = self.cost, metrics = metrics)
     self.k_model.fit(x, y, epochs = epochs, batch_size = batch_size, validation_split = 0.2, verbose = 2)
 
-  def evaluate(self, x, y):
+  def evaluate(self, x, y, verbose = True):
     """wrapper for Sequential evaluate"""
-    return self.k_model.evaluate(x, y)
+    start = time()
+    evaluation = self.k_model.evaluate(x, y, verbose = 2)
+    result = "Test evaluation\n - {0}s".format(round(time() - start))
+    for name, evaluation in zip(test.k_model.metrics_names, evaluation):
+      result += (" - test_{0}: {1}".format(name, round(evaluation, 4)))
+    if verbose: print (result)
+    return evaluation
 
   def predict(self, x):
     """wrapper for Sequential predict"""
@@ -98,6 +103,7 @@ def load_mnist():
   return (x_train, y_train), (x_test, y_test)
 
 if __name__ == "__main__":
-  test = Network([Input(784), Dense(30), Dense(10)])
+  test = NN([Input(784), Dense(100), Dense(10)])
   (x_train, y_train), (x_test, y_test) = load_mnist()
   test.train(x_train, y_train)
+  test.evaluate(x_train, y_train)
