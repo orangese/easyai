294df2e562e40307237f506074aa83d003315e44
Ryan <Ryan@Johns-MacBook-Pro.local>
2019-07-29 20:36:42 -0400

    updating mnist loader and creating cnn example

--- easyai/support/datasets.py
+++ easyai/support/datasets.py
@@ -13,14 +13,28 @@ from easyai.core import Static_Interface
 class Builtins(Static_Interface):
 
   @staticmethod
-  def load_mnist():
+  def load_mnist(mode = "mlp"):
     """loads MNIST data for use by a keras Sequential object"""
     (x_train, y_train), (x_test, y_test) = K.datasets.mnist.load_data()
 
-    x_train = (x_train.reshape(x_train.shape[0], -1) / 255).astype("float32")
-    y_train = K.utils.to_categorical(y_train, 10).astype("float32")
+    x_train = (x_train / 255).astype("float32")
+    x_test = (x_test / 255).astype("float32")
 
-    x_test = (x_test.reshape(x_test.shape[0], -1) / 255).astype("float32")
-    y_test = K.utils.to_categorical(y_test, 10).astype("float32")
+    y_train = y_train.astype("float32")
+    y_test = y_test.astype("float32")
+
+    if mode == "mlp":
+      x_train = x_train.reshape(x_train.shape[0], -1)
+      x_test = x_test.reshape(x_test.shape[0], -1)
+    elif mode == "conv":
+      if K.backend.image_data_format() == "channels_first":
+        x_train.resize(x_train.shape[0], 1, *x_train.shape[1:])
+        x_test.resize(x_test.shape[0], 1, *x_test.shape[1:])
+      else:
+        x_train.resize(*x_train.shape, 1)
+        x_test.resize(*x_test.shape, 1)
+
+    y_train = K.utils.to_categorical(y_train, 10)
+    y_test = K.utils.to_categorical(y_test, 10)
 
     return (x_train, y_train), (x_test, y_test)
--- easyai/support/examples.py
+++ easyai/support/examples.py
@@ -7,6 +7,9 @@ Program that implements easyai.support.datasets and easyai.core in examples like
 
 """
 
+from tkinter import *
+from PIL import Image, ImageDraw
+import numpy as np
 from easyai.core import *
 from easyai.support.datasets import Builtins
 
@@ -14,13 +17,58 @@ class MNIST(Static_Interface):
 
   @staticmethod
   def mlp():
-    """MNIST multi-layer perceptron network. One sigmoidal hidden layer of 100 neurons and MSE cost"""
+    """MNIST multi-layer perceptron network."""
+    Error_Handling.suppress_tf_warnings()
+
+    (x_train, y_train), (x_test, y_test) = Builtins.load_mnist(mode = "mlp")
+
     mlp = NN([Input(784), Dense(100), Dense(10)])
-    (x_train, y_train), (x_test, y_test) = Builtins.load_mnist()
     print (mlp.summary())
-    mlp.train(x_train, y_train)
+
+    mlp.train(x_train, y_train, lr = 3.0)
     mlp.evaluate(x_test, y_test)
 
   @staticmethod
   def cnn():
     """MNIST convolutional network."""
+    Error_Handling.suppress_tf_warnings()
+    (x_train, y_train), (x_test, y_test) = Builtins.load_mnist(mode = "conv")
+
+    mlp = NN([Input((28, 28)), Conv((5, 5), 20), Pooling((2, 2)), Dense(100), Dense(10)])
+    print (mlp.summary())
+
+    mlp.train(x_train, y_train, lr = 0.1)
+    mlp.evaluate(x_test, y_test)
+
+  """--BELOW NOT SUPPORTED--"""
+  @staticmethod
+  def draw(fileName):
+    width = 200
+    height = 200
+    white = (255, 255, 255)
+
+    def save():
+      image1.save(fileName)
+
+    def drawIm(event):
+      x1, y1 = (event.x - 5), (event.y - 5)
+      x2, y2 = (event.x + 5), (event.y + 5)
+      cv.create_oval(x1, y1, x2, y2, width=5, fill="black")
+      draw.line(((x2, y2), (x1, y1)), fill="black", width=10)
+
+    root = Tk()
+    cv = Canvas(root, width=width, height=height, bg="white")
+    cv.pack()
+    image1 = PIL.Image.new("RGB", (width, height), white)
+    draw = ImageDraw.Draw(image1)
+    cv.bind("<B1-Motion>", drawIm)
+    button = Button(text="save", command=save)
+    button.pack()
+    root.mainloop()
+
+  @staticmethod
+  def getActivation(fileName):
+    img = Image.open(fileName)
+    img = img.resize((28, 28))
+    img = np.take(np.asarray(img), [0], axis=2).reshape(28, 28)
+    return np.abs(img - 255)
