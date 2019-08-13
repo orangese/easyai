ef4368246d784f646300f8ea4163023529521882
Ryan Park <22parkr@millburn.org>
2019-08-13 14:57:44 -0400

    adding support for cpu and python3.7

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name = "easyai",
-      version = "0.0.1b",
+      version = "0.1b1",
       description = "AI made easy.",
       long_description = open("README.md").read(),
       url = "https://github.com/orangese/easyai",
       author = "Ryan Park",
       author_email = "22parkr@millburn.org",
       license = None,
-      python_requires = "<3.7.0",
-      install_requires = ["numpy", "scipy", "tensorflow-gpu==1.8.0", "keras", "matplotlib", "pandas", "pillow",
-                          "requests", "xlrd", "six"],
-      packages = ["easyai"],
+      python_requires = ">=3.6.0",
+      install_requires = ["numpy", "scipy", "keras", "matplotlib", "pandas", "pillow", "requests", "xlrd", "six"],
+      packages = find_packages(),
+      extras_require = {"gpu": ["tensorflow-gpu==1.8.0"], "cpu": ["tensorflow"]},
       zip_safe = False)
