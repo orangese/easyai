db761c0cddd56b07f007590a8dedfe8899450dac
Ryan Park <22parkr@millburn.org>
2019-08-20 09:14:41 -0400

    initializing models.py

---
+++ easyai/support/models.py
@@ -0,0 +1,14 @@
+
+"""
+
+"easyai.support.models.py"
+
+Holds links to pre-trained easyai models stored on Google Drive.
+
+"""
+
+from easyai.core import Static_Interface
+
+class Art(Static_Interface):
+
+  fast_nst = {}
